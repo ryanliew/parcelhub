030ed24552c95ab5ebeb5453fa494950178ef45c
Ryan Liew <fatal-fates@hotmail.com>
2018-08-21 20:13:26 +0800

    Corrected user verification logic

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -35,29 +35,54 @@ class SocialController extends Controller
 
         $authUser = $this->findOrCreateUser($socialUser);
 
-        // Generate token used for email verification
-        $token = new UserToken([
-            'token' => str_random(60),
-            'expire_at' => Carbon::now()->addMinute(5)
-        ]);
+        // User is new or not verified
+        if($authUser['is_create'] || !$authUser['user']->verified) {
+            // Generate token used for email verification
+            $token = new UserToken([
+                'token' => str_random(60),
+                'expire_at' => Carbon::now()->addMinute(5)
+            ]);
 
-        $authUser->tokens()->save($token);
+            $authUser['user']->tokens()->save($token);
 
-        User::admin()->first()->notify(new UserRegisteredNotification($authUser));
-        // Send email verification to users email
-        $authUser->notify(new AccountVerificationNotification($authUser));
-		
-        return view('user.verify')->with([
-            'message' => trans('auth.token_not_verify'),
-            'id' => $authUser->id,
-            'banned' => false
-        ]);
+            User::admin()->first()->notify(new UserRegisteredNotification($authUser['user']));
+            // Send email verification to users email
+            $authUser['user']->notify(new AccountVerificationNotification($authUser['user']));
+    		
+            return view('user.verify')->with([
+                'message' => trans('auth.token_not_verify'),
+                'id' => $authUser['user']->id,
+                'banned' => false
+            ]);
+        }
+
+        // Not new user
+        Auth::login($authUser['user']);
+
+        // Check if user is verified and approved by admin
+        if($authUser['user']->verified && $authUser['user']->is_approved) {
+            if($authUser['user']->hasRole('admin')) {
+                return redirect()->intended('dashboard');
+            }
+        } else {
+            // User is not verified / approved by admin
+            $id = $authUser['user']->id;
+            $message = $authUser['user']->verified ? "You have been restricted from accessing the system. Please contact the administrator for help." : trans('auth.token_not_verify');
+            $banned = $authUser['user']->verified && !$authUser['user']->is_approved ;
+            Auth::logout();
+
+            return view('user.verify')->with([
+                'message' => $message,
+                'id' => $id,
+                'banned' => $banned
+            ]);
+        }
     }
 
     public function findOrCreateUser($user)
     {
         if($authUser = User::where('email', '=', $user->email)->first()) {
-            return $authUser;
+            return ['user' => $authUser, 'is_create' => false];
         }
 
         $user = User::create([
@@ -70,6 +95,6 @@ class SocialController extends Controller
 
         $user->attachRole($role);
 
-        return $user;
+        return ['user' => $user, 'is_create' => true];
     }
 }
